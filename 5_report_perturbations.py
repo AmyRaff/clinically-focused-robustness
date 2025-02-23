8925022601c277015ac7d0caec0f58a7440212cf
AmyRaff <51639064+AmyRaff@users.noreply.github.com>
2025-02-23 13:26:07 +0000

    Update 5_report_perturbations.py

--- 5_report_perturbations.py
+++ 5_report_perturbations.py
@@ -123,7 +123,7 @@ def get_label_indices(label):
     elif label == 'Cardiomegaly':
         indices = [14]
     elif label == 'Cancer':
-        indices = [1, 2, 3, 4]# , 5] NOTE!!!
+        indices = [1, 2, 3, 4, 5]# NOTE!!!
     elif label == 'Pneumothorax':
         indices = [15, 16]
     elif label == 'Pneumonia':
@@ -133,7 +133,7 @@ def get_label_indices(label):
         indices = [10, 11, 12, 13]
     return indices
 
-def get_outer_perturbs(label, num):
+def get_fp_perturbs(label, num):
     label_indices = []
     for id in train_info['id']:
         im = image_info[image_info['id'] == id]['name'].values[0]
@@ -154,6 +154,8 @@ def get_outer_perturbs(label, num):
             filename = filenames[i]
             report_concs = full_report_concepts[i]
             correct_labels = present_labels[i]
+            if len(correct_labels) > 1: # NOTE
+                continue
             # num_perturbations = 2 # NOTE: num
             original = report_concs
             outer_class_perturbs = []
@@ -186,10 +188,67 @@ def get_outer_perturbs(label, num):
                             new_labels.append(new_label)
                             outer_class_perturbs.append(concs)
             used_filenames.append(f'{label}/{filename}')
+            assert len(outer_class_perturbs) == num
             all_perturbs.append(outer_class_perturbs)
             # check perturbations belong to the correct random classes
             for i in range(len(outer_class_perturbs)):
                 assert new_labels[i] in check_label(outer_class_perturbs[i])
+        
+    return all_perturbs, used_filenames
+
+
+def get_fn_perturbs(label, num):
+    label_indices = []
+    for id in train_info['id']:
+        im = image_info[image_info['id'] == id]['name'].values[0]
+        cls = class_info[class_info['id'] == id]['label'].values[0]
+        # filter only for images of the current class that are in the test set
+        if all_labels[cls] == label:
+            is_train = train_info[train_info['id'] == id]['is_train'].values[0]
+            if im in filenames and is_train == 0:
+                idx = filenames.index(im)
+                label_indices.append(idx)
+            else:
+                label_indices.append(None)
+    
+    used_filenames, all_perturbs = [], []
+
+    for i in label_indices:
+        if i is not None:
+            filename = filenames[i]
+            report_concs = full_report_concepts[i]
+            correct_labels = present_labels[i]
+            if len(correct_labels) < 2: # NOTE
+                continue
+            # num_perturbations = 2 # NOTE: num
+            original = report_concs
+            outer_class_perturbs = []
+            rem_labels = []
+            # get n random perturbations of the concepts of another class
+            while len(outer_class_perturbs) < num:
+                # randomly select a single true label
+                single_label = random.choice(correct_labels)
+                rem_labels.append(single_label)
+                #if new_label != label:
+                indices = get_label_indices(single_label)
+                #a = [random.getrandbits(1) for a in range(len(indices))]
+                # NOTE: dont want too many positive
+                #if a != [0] * len(indices) and sum(a) < 3:
+                concs = original.copy()
+                # set everything for single label to 0
+                for c in range(len(concs)):
+                    if c in indices:
+                        concs[c] = 0
+                outer_class_perturbs.append(concs)
+            used_filenames.append(f'{label}/{filename}')
+            assert len(outer_class_perturbs) == num
+            all_perturbs.append(outer_class_perturbs)
+            # check perturbations belong to the correct random classes
+            for i in range(len(outer_class_perturbs)):
+                #print(rem_labels[i])
+                #print(outer_class_perturbs[i])
+                #print(check_label(outer_class_perturbs[i]))
+                assert rem_labels[i] not in check_label(outer_class_perturbs[i])
         
     return all_perturbs, used_filenames
             
@@ -197,48 +256,73 @@ def get_outer_perturbs(label, num):
 every_file, every_perturbation = [], []
 for l in tqdm(all_labels):
     print(l)
-    # NOTE: changed to 20 to ensure existing concepts captured in next file
+    # NOTE: need 6, do more here so can capture later
     # num_perturbations = 10
-    print('Generating inter-class perturbations...')
+    # print('Generating inter-class perturbations...')
     if l != 'Healthy' and l != 'Cardiomegaly':
+        print('Generating inter-class perturbations...')
         inner_perturbs, inner_files = get_inter_class_perturbs(l, 2)
-        outer_perturbs, outer_files = get_outer_perturbs(l, 20)
-        assert len(inner_files) == len(inner_perturbs)
-        
-        # want 20 - n outer-class perturbations
-        # num_perturbations -= len(inner_perturbs[0])
+        print('Generating fp perturbations...')
+        fp_perturbs, fp_files = get_fp_perturbs(l, 10)
+        print('Generating fn perturbations...')
+        fn_perturbs, fn_files = get_fn_perturbs(l, 10)
         
     else:
-        inner_perturbs = None
-        outer_perturbs, outer_files = get_outer_perturbs(l, 20)
+        inner_files, inner_perturbs = None, None
+        print('Generating fp perturbations...')
+        fp_perturbs, fp_files = get_fp_perturbs(l, 11)
+        print('Generating fn perturbations...')
+        fn_perturbs, fn_files = get_fn_perturbs(l, 11)
     
-    print('Generating outer-class perturbations...')
-    assert len(outer_files) == len(outer_perturbs)
     
     print('Merging and writing perturbations...')
+    pairs = []
     if inner_perturbs is not None:
-        assert inner_files == outer_files
-
-        all_perturbations = []
-        for i in range(len(inner_files)):
-            perturbations = []
-            for inner in inner_perturbs[i]:
-                perturbations.append(inner)
-            for outer in outer_perturbs[i]:
-                perturbations.append(outer)
-            #assert len(perturbations) == 20 # NOTE
-            all_perturbations.append(perturbations)
-    else:
-        all_perturbations = outer_perturbs
+        for i in range(len(inner_perturbs)):
+            pairs.append((inner_files[i], inner_perturbs[i]))
+    for i in range(len(fp_perturbs)):
+        pairs.append((fp_files[i], fp_perturbs[i]))
+    for i in range(len(fn_perturbs)):
+        pairs.append((fn_files[i], fn_perturbs[i]))
     
-    assert len(all_perturbations) == len(outer_files)
-    for file in outer_files:
+    #print(pairs)
+    #pairs = set(pairs)
+    all_files, all_perturbs = [], []
+    for file, perturb in pairs:
+        perturbs = []
+        for p in range(len(pairs)):
+            if pairs[p][0] == file:
+                curr = pairs[p][1]
+                for c in curr:
+                    perturbs.append(c)
+        all_files.append(file)
+        all_perturbs.append(perturbs)
+    
+    # if inner_perturbs is not None:
+    #     assert inner_files == fp_files
+
+    #     all_perturbations = []
+    #     for i in range(len(inner_files)):
+    #         perturbations = []
+    #         for inner in inner_perturbs[i]:
+    #             perturbations.append(inner)
+    #         for fp in fp_perturbs[i]:
+    #             perturbations.append(fp)
+    #         #assert len(perturbations) == 20 # NOTE
+    #         all_perturbations.append(perturbations)
+    # else:
+    #     all_perturbations = fp_perturbs
+    
+    # print(len(all_files))
+    assert len(all_perturbs) == len(all_files)
+    for file in all_files:
         every_file.append(file)
-    for perturb in all_perturbations:
-        every_perturbation.append(perturb)
+    if all_perturbs is not None:
+        for perturb in all_perturbs:
+            every_perturbation.append(perturb)
     
 print(len(every_perturbation))
-with open('new_twenty_test_concept_perturbations.txt', 'w') as f:
+with open('aaa.txt', 'w') as f:
     for i in range(len(every_perturbation)):
         f.write(f'{every_file[i]}\n')
         f.write(f'{every_perturbation[i]}\n\n')
